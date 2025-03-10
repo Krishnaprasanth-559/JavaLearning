41a198506480139cec9ce11094f187df5ff51275
Krishnaprasanth-559 <Krishnaprasanth.sambangi@gmail.com>
2025-03-10 23:10:54 +0530

    10 March commit

---
+++ src/ex_01_Java_Basic/Lab002_Comments.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+
+        //Below is println command
+
+        System.out.println("Java comments");
+
+        /*
+        commenting multiple line of code
+        System.out.println("Commented code");
+         */
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab003_Keywords.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Keywords
+{
+    public static void main(String[] args) {
+
+        System.out.println("Hello How are you");
+
+        System.out.print("Doing");
+    }
+}
