ed2829a34399af393ab54caf68359bbc81e564e9
Krishnaprasanth-559 <Krishnaprasanth.sambangi@gmail.com>
2025-03-12 00:47:06 +0530

    11 March commmit

---
+++ src/ex_02_Java_Basic/Lab010_Printf.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basic;
+
+public class Lab010_Printf {
+    public static void main(String[] args) {
+
+        int a=10, b=20;
+
+        System.out.printf("%d+%d",a,b);
+
+    }
+}
---
+++ src/ex_02_Java_Basic/Lab011_Printf9Table.java
@@ -0,0 +1,24 @@
+package ex_02_Java_Basic;
+
+public class Lab011_Printf9Table {
+    public static void main(String[] args) {
+
+        int table=9;
+        int a=1,b=2,c=3,d=4;
+        int e=5,f=6,g=7,h=8,i=9,j=10;
+        System.out.printf("%d*%d=%d",table,a,(table*a));
+        System.out.printf("%d*%d=%d",table,b,(table*b));
+        System.out.printf("%d*%d=%d",table,c,(table*c));
+        System.out.printf("%d*%d=%d",table,d,(table*d));
+        System.out.printf("%d*%d=%d",table,e,(table*e));
+        System.out.printf("%d*%d=%d",table,f,(table*f));
+        System.out.printf("%d*%d=%d",table,g,(table*g));
+        System.out.printf("%d*%d=%d",table,h,(table*h));
+        System.out.printf("%d*%d=%d",table,i,(table*i));
+        System.out.printf("%d*%d=%d",table,j,(table*j));
+    }
+}
+
+
+
+
